fab01f224a40286e9a9b2b98696792d459b303b9
ysak-y <yoshiaki.0614@gmail.com>
2021-08-11 23:12:52 +0900

    Update

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -3,7 +3,8 @@
 
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/colors ~/.vim/colors
-ln -sf ~/.bash_profile ~/.bash_profile
+ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
+ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 
 mkdir ~/.vim/bundle
 
